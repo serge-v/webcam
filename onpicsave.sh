39f95f89515484040fd7139840d1ff6eb31b0bac
Serge Voilokov <serge0x76@gmail.com>
2015-11-20 19:56:32 +0000

    Fix errors in event handler.

--- onpicsave.sh
+++ onpicsave.sh
@@ -1,7 +1,11 @@
+#!/bin/sh
+
 set -e
+set -x
 
 LOG=/tmp/evt.log
-date >> $LOG
+URL=`cat /root/.config/motion-conf/ftp-url.txt`
+
 echo file: $1 >> $LOG
-ftp -u '`cat ~/.config/motion-conf/ftp-url.txt`' $1 >> $LOG
+ftp -u "$URL" $1 >> $LOG
 find /tmp -name '*.jpg' -mmin +2 -print -delete >> $LOG
