e05045099a553979756dc67d60c683416566914e
Serge Voilokov <serge0x76@gmail.com>
2015-12-03 02:51:16 +0000

    Annotate hourly.

--- annotate.sh
+++ annotate.sh
@@ -2,8 +2,12 @@
 #set -x
 set -e
 
-for f in 20151128*.jpg ; do
+cd ~/cam
+
+for f in *.jpg ; do
     text=`echo $f | cut -d'-' -f 1 -f 2 | sed s/.jpg//`
     echo $text
-    [ ! -f ann-$f ] && /usr/local/bin/magick $f -rotate 180 -font sourcecode-light.ttf -fill red -pointsize 100 -draw "text 60,60 \'$text\'" ann-$f
+    /usr/local/bin/magick $f -rotate 180 -font sourcecode-light.ttf -fill red -pointsize 100 -draw "text 60,60 \'$text\'" ann-$f
+    rm $f
 done
+
--- encode.sh
+++ encode.sh
@@ -1,1 +1,16 @@
-ffmpeg -y -framerate 8 -pattern_type glob -i 'ann-20151128*.jpg' -c:v libxvid 20151128.mp4
+
+set -e
+set -x
+
+cd ~/cam
+
+day=`ls -1 ann-20*.jpg | sed 's/ann-//;s/-.*//' | uniq | head -1`
+
+outfile=${day}.mp4
+echo encode day ${day} to ${outfile}?
+
+read aa
+ffmpeg -y -framerate 8 -pattern_type glob -i "ann-${day}*.jpg" -c:v libxvid ${outfile}
+
+read aa
+rm ann-${day}*.jpg
