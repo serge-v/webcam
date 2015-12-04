efe155c6525659cc4eace7b2b48dad6555470fd1
Serge Voilokov <serge0x76@gmail.com>
2015-12-04 01:13:15 +0000

    Fix mask.

--- annotate.sh
+++ annotate.sh
@@ -4,10 +4,9 @@ set -e
 
 cd ~/cam
 
-for f in *.jpg ; do
+for f in 20*.jpg ; do
     text=`echo $f | cut -d'-' -f 1 -f 2 | sed s/.jpg//`
     echo $text
     /usr/local/bin/magick $f -rotate 180 -font sourcecode-light.ttf -fill red -pointsize 100 -draw "text 60,60 \'$text\'" ann-$f
     rm $f
 done
-
