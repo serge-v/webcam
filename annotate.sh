3bc498e7d1a65414349d13fe958d45ed02b03fa4
Serge Voilokov <serge0x76@gmail.com>
2015-12-15 04:40:06 +0000

    Reduce font.

--- annotate.sh
+++ annotate.sh
@@ -4,9 +4,12 @@ set -e
 
 cd ~/cam
 
+find ~/cam -name '*.jpg' -size 0 -delete
+
 for f in 20*.jpg ; do
     text=`echo $f | cut -d'-' -f 1 -f 2 | sed s/.jpg//`
     echo $text
-    /usr/local/bin/magick $f -rotate 180 -font sourcecode-light.ttf -fill red -pointsize 100 -draw "text 60,60 \'$text\'" ann-$f
+    /usr/local/bin/magick $f -rotate 180 -font sourcecode-light.ttf -fill red -pointsize 40 -draw "text 60,60 \'$text\'" ann-$f
     rm $f
 done
+
